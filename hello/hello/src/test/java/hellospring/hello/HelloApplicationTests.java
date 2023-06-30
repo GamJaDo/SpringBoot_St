13248fb78f45154aece6193fe99ae2f52a73a034
GamJaDo <kim0615157@gmail.com>
2023-06-30 23:49:50 +0900

    no message

--- hello/hello/src/test/java/hellospring/hello/HelloApplicationTests.java
+++ hello/hello/src/test/java/hellospring/hello/HelloApplicationTests.java
@@ -1,10 +1,10 @@
-package hello.hellospring;
+package hellospring.hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
+class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
